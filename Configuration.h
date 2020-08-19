77c2ee1ac2a3103f9009e7266218bf7305109b2b
masterdot <masterdot@experimentalgrounds.org>
2020-08-19 19:15:40 +0200

    TFT35 is now working!
    after sooo many trys, the tft35 is now working again.
    it looks like the build target had an effect on the function. 
    i did use now stm32f103rc_btt and it does connect.

--- Configuration.h
+++ Configuration.h
@@ -109,18 +109,23 @@
 #define SERIAL_PORT -1
 
 /**
- * Select a secondary serial port on the board to use for communication with the host.
- * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
+ * This setting determines the communication speed of the printer.
+ *
+ * 250000 works in most cases, but you might try a lower speed if
+ * you commonly experience drop-outs during host printing.
+ * You may try up to 1000000 to speed up SD file transfer.
+ *
+ * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
 
-#define SERIAL_PORT_2 1
+#define BAUDRATE 115200
 
 /**
  * Select a secondary serial port on the board to use for communication with the host.
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
 
-#define SERIAL_PORT_3 2
+#define SERIAL_PORT_2 2
 
 /**
  * This setting determines the communication speed of the printer.
